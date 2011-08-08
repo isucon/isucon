4009fe1c929c6bb1f24cb7a316ef931f611d09a9
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-08-08 14:48:18 +0900

    add default cnf, and schema

---
+++ webapp/config/database/isucon.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE IF NOT EXISTS isucon DEFAULT CHARACTER SET 'utf8';
+
+CREATE USER 'isuconapp'@'%' IDENTIFIED BY 'isunageruna';
+GRANT ALL ON isucon.* TO 'isuconapp'@'%';
+
+CREATE TABLE IF NOT EXISTS isucon.article (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(128) NOT NULL,
+  body  VARCHAR(4096) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  modified_at TIMESTAMP NULL
+) ENGINE=InnoDB;
+
+CREATE TABLE IF NOT EXISTS isucon.comment (
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  article INT NOT NULL,
+  name VARCHAR(64),
+  body VARCHAR(1024) NOT NULL,
+  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  INDEX (article, id DESC),
+  # INDEX (created_at DESC, article)
+  INDEX (article, created_at DESC)
+) ENGINE=InnoDB;
