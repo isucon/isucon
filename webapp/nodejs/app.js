02e6ae6df2ae27598bbdc5dbeea60e50887a996a
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-08-09 12:39:06 +0900

    implement comment posting

--- webapp/nodejs/app.js
+++ webapp/nodejs/app.js
@@ -82,16 +82,49 @@ app.get('/', function(req, res){
 });
 
 app.get('/post', function(req, res){
-  loadSidebarData(function(err, sidebarHtml){
-    if (err) {error_handle(req, res, err); return;}
-    res.render('post');
-  });
+  res.render('post');
 });
 
 app.post('/post', function(req, res){
+  var article_post_query = 'INSERT INTO article SET title=?, body=?';
   var title = req.body.title;
   var body = req.body.body;
-  // do INSERT and res.redirect ....
+  dbclient.query(article_post_query, [title,body], function(err, results){
+    if (err) {error_handle(req, res, err); return;}
+    res.redirect('/');
+  });
+});
+
+app.get('/article/:articleid', function(req, res){
+  var article_query = 'SELECT id,title,body,created_at FROM article WHERE id=?';
+  var comments_query = 'SELECT name,body,created_at FROM comment WHERE article=? ORDER BY id';
+  var articleid = req.params.articleid;
+  loadSidebarData(function(err, sidebarHtml){
+    if (err) {error_handle(req, res, err); return;}
+    dbclient.query(article_query, [articleid], function(err, results){
+      if (err) {error_handle(req, res, err); return;}
+      if (results.length != 1) {
+        res.send(404);
+        return;
+      }
+      var article = results[0];
+      dbclient.query(comments_query, [articleid], function(err, results){
+        if (err) {error_handle(req, res, err); return;}
+        res.render('article', {sidebar: sidebarHtml, article: article, comments: results});
+      });
+    });
+  });
+});
+
+app.post('/comment/:articleid', function(req, res){
+  var comment_post_query = 'INSERT INTO comment SET article=?, name=?, body=?';
+  var article = req.params.articleid;
+  var name = req.body.name;
+  var body = req.body.body;
+  dbclient.query(comment_post_query, [article,name,body], function(err, results){
+    if (err) {error_handle(req, res, err); return;}
+    res.redirect('/article/' + article);
+  });
 });
 
 app.listen(5000);
