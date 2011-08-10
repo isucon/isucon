d4e905e73ff9a7b301042fd27c145f076e18eba4
Masahiro Nagano <kazeburo@gmail.com>
2011-08-11 00:37:02 +0900

    perl sample apps

---
+++ webapp/perl/lib/Isucon.pm
@@ -0,0 +1,76 @@
+package Isucon;
+
+use strict;
+use warnings;
+use utf8;
+use Kossy;
+use DBI;
+
+sub dbh {
+    my $self = shift;
+    DBI->connect_cached('dbi:mysql:isucon;host=127.0.0.1','isucon','isucon',{
+        RaiseError => 1,
+        PrintError => 0,
+        ShowErrorStatement => 1,
+        AutoInactiveDestroy => 1,
+        mysql_enable_utf8 => 1
+    });
+}
+
+filter 'recent_commented_articles' => sub {
+    my $app = shift;
+    sub {
+        my ( $self, $c )  = @_;
+        $c->stash->{recent_commented_articles} = $self->dbh->selectall_arrayref(
+            'SELECT DISTINCT a.id, a.title FROM comment c, article a 
+             WHERE c.article = a.id ORDER BY c.created_at DESC LIMIT 10',
+            { Slice => {} });
+        $app->($self,$c);
+    }
+};
+
+get '/' => [qw/recent_commented_articles/] => sub {
+    my ( $self, $c )  = @_;
+    my $rows = $self->dbh->selectall_arrayref(
+        'SELECT id,title,body,created_at FROM article ORDER BY id DESC LIMIT 10',
+        { Slice => {} });
+    $c->render('index.tx', { articles => $rows });
+};
+
+get '/article/:articleid' => [qw/recent_commented_articles/] => sub {
+    my ( $self, $c )  = @_;
+    my $article = $self->dbh->selectrow_hashref(
+        'SELECT id,title,body,created_at FROM article WHERE id=?',
+        {}, $c->args->{articleid});
+    my $comments = $self->dbh->selectall_arrayref(
+        'SELECT name,body,created_at FROM comment WHERE article=? ORDER BY id', 
+        { Slice => {} }, $c->args->{articleid});
+    $c->render('article.tx', { article => $article, comments => $comments });
+};
+
+get '/post' => [qw/recent_commented_articles/] => sub {
+    my ( $self, $c )  = @_;
+    $c->render('post.tx');
+};
+
+post '/post' => sub {
+    my ( $self, $c )  = @_;
+    my $sth = $self->dbh->prepare('INSERT INTO article SET title = ?, body = ?');
+    $sth->execute($c->req->param('title'), $c->req->param('body'));
+    $c->res->redirect($c->req->uri_for('/'));
+};
+
+post '/comment/:articleid' => sub {
+    my ( $self, $c )  = @_;
+
+    my $sth = $self->dbh->prepare('INSERT INTO comment SET article = ?, name =?, body = ?');
+    $sth->execute(
+        $c->args->{articleid},
+        $c->req->param('name'), 
+        $c->req->param('body')
+    );
+    $c->res->redirect($c->req->uri_for('/article/'.$c->args->{articleid}));
+};
+
+1;
+
