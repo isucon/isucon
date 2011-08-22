1e62642f05c41bc99faded25dfc64805885f069c
TAGOMORI Satoshi <tagomoris@gmail.com>
2011-08-22 16:40:51 +0900

    fix plackup path with system perl module

--- webapp/perl/etc/app.sh
+++ webapp/perl/etc/app.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $HOME/isucon/webapp/perl
-exec perl -Mlib=extlib/lib/perl5 extlib/bin/plackup -s Starman -E production --preload-app app.psgi
+exec perl -Mlib=extlib/lib/perl5 /usr/bin/plackup -s Starman -E production --preload-app app.psgi
