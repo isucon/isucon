0d65052157c614ee4c31c8494e62a02a7234310b
Masahiro Nagano <kazeburo@gmail.com>
2011-08-25 01:44:40 +0900

    leftjoin=>innerjoin, requries newer DBI

--- webapp/perl/lib/Isucon.pm
+++ webapp/perl/lib/Isucon.pm
@@ -36,7 +36,7 @@ filter 'recent_commented_articles' => sub {
     sub {
         my ( $self, $c )  = @_;
         $c->stash->{recent_commented_articles} = $self->dbh->selectall_arrayref(
-            'SELECT a.id, a.title FROM comment c LEFT JOIN article a ON c.article = a.id 
+            'SELECT a.id, a.title FROM comment c INNER JOIN article a ON c.article = a.id 
             GROUP BY a.id ORDER BY MAX(c.created_at) DESC LIMIT 10',
             { Slice => {} });
         $app->($self,$c);
