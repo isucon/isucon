8c70242a2f1303a82d0e62cb1a62d582801ce7ad
karupanerura(isucon) <karupa@cpan.org>
2016-07-30 06:08:30 +0000

    updated env.sh

--- standalone/env.sh
+++ standalone/env.sh
@@ -2,7 +2,5 @@
 
 USERNAME=isucon
 USER_HOME=/home/isucon
-NODE_VERSION=v0.4.11
 
-export PATH=$PATH:$USER_HOME/.nvm/$NODE_VERSION/bin
-export NODE_PATH=$NODE_PATH:$USER_HOME/node_modules/
+export PATH=$HOME/.ndenv/shims:$HOME/.ndenv/bin:$PATH
